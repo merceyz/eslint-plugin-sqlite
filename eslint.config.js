144b89517d91e1901f2a9845a992d730d90f6c2c
merceyz <merceyz@users.noreply.github.com>
2024-08-11 16:19:48 +0200

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+// @ts-check
+
+import fs from "node:fs";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+	eslint.configs.recommended,
+	...tseslint.configs.strictTypeChecked,
+	...tseslint.configs.stylisticTypeChecked,
+	prettier,
+	{
+		languageOptions: {
+			parserOptions: {
+				projectService: true,
+			},
+		},
+		linterOptions: {
+			reportUnusedDisableDirectives: "error",
+		},
+		rules: {
+			"no-undef": "off",
+		},
+	},
+	{
+		ignores: (
+			await fs.promises.readFile(
+				new URL("./.prettierignore", import.meta.url),
+				"utf8",
+			)
+		)
+			.split("\n")
+			.filter((line) => line.trim() && !line.startsWith("#")),
+	},
+);
